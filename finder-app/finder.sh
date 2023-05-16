0298ace2bd8f8b8bbbae5a296a0e2894819207b9
Chris Strong <chris.strong@alstomgroup.com>
2023-05-16 15:33:13 -0400

    Assignment 1 scripts and changes

---
+++ finder-app/finder.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+search_dir=$1
+search_string=$2
+
+if [ -z "$search_dir" ]
+then
+    echo "Search directory not specified!"
+    exit 1
+fi
+
+if [ -z "$search_string" ]
+then
+    echo "Search string not specified!"
+    exit 1
+fi
+
+if [ -d "$search_dir" ]
+then
+    file_count=$(find $1 -type f | wc -l)
+    match_count=$(grep -roh $search_string $search_dir | wc -w)
+    echo "The number of files are $file_count and the number of matching lines are $match_count"
+
+else
+    echo "Directory specified does not exist!"
+    exit 1
+fi
---
+++ finder-app/writer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+write_path=$1
+write_string=$2
+
+if [ -z "$write_path" ];
+then
+    echo "Write directory not specified!"
+    exit 1
+fi
+
+if [ -z "$write_string" ];
+then
+    echo "Write string not specified!"
+    exit 1
+fi
+
+if [ ! -d $(dirname $write_path) ];
+then
+    mkdir -p $(dirname $write_path)
+fi
+
+echo $write_string > $write_path
+
+if [ ! $? -eq 0 ];
+then
+    echo "Write failed!"
+    exit 1
+fi
